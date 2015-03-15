f951ac2dba963fff1b7ec9119721811e238e08c5
Manan <manan04upadhyay@gmail.com>
2015-03-14 17:59:24 -0700

    First Commit

---
+++ src/correct.py
@@ -0,0 +1,181 @@
+import sys,nltk,subprocess
+from nltk import compat
+
+def getSuffix(word,type):
+    wlen = len(word)
+    return word[wlen-type:wlen]
+
+def formatInput(inFile,outFile,mSet):
+    outList = []
+    predList = []
+    ou = open(outFile,'w')
+    tr = open(inFile,'r')
+    for line in tr:
+        # print(".")
+        # create tags;
+        # toTagLine = nltk.word_tokenize(line)
+        # tagList = nltk.pos_tag(toTagLine)
+
+        splitLine = line.split()
+        tagList = nltk.pos_tag(splitLine)
+
+        splitLineSet = set()
+        for item in splitLine:
+            splitLineSet.add(str(item))
+
+        if ("its" not in splitLineSet and
+            "it\'s" not in splitLineSet and
+            "too" not in splitLineSet and
+            "to" not in splitLineSet and
+            "your" not in splitLineSet and
+            "you\'re" not in splitLineSet and
+            "their" not in splitLineSet and
+            "they\'re" not in splitLineSet and
+            "loose" not in splitLineSet and
+            "lose" not in splitLineSet):
+            continue
+
+        splitLength = len(splitLine)
+        prevWord1 = "^bos$"
+        prevWord2 = "^bos$"
+        prevClassLabel1 = "^cla$$"
+        prevClassLabel2 = "^cla$$"
+        nextWordClass1 = "^cla$$"
+        nextWordClass2 = "^cla$$"
+        curWord = ""
+        nextWord = ""
+        curClassLabel = ""
+        suffix2 = ""
+        suffix3 = ""
+        reconLine = ""
+
+        for i in range(splitLength):
+            tagLine = ""
+            outTagLine = ""
+            if str(splitLine[i]) not in mSet:
+                continue
+
+            # handle cur word;
+            curWord = str(splitLine[i])
+            suffix2 = getSuffix(curWord,2)
+            suffix3 = getSuffix(curWord,3)
+            curWordTag = tagList[i]
+            curClassLabel = str(curWordTag[1])
+
+            #handle prev word;
+            if(i == 0):
+                prevWord1 = "^bos$"
+                prevWord2 = "^bos$"
+
+            else:
+                prevWord1 = splitLine[i-1]
+                prevWordTag1 = tagList[i-1]
+                prevClassLabel1 = str(prevWordTag1[1])
+                if (i == 1):
+                    prevWord2 = "^bos$"
+                else:
+                    prevWord2 = splitLine[i-2]
+                    prevWordTag2 = tagList[i-2]
+                    prevClassLabel2 = str(prevWordTag2[1])
+
+            # handle next;
+            if (i  == splitLength - 1):
+                nextWord1 = "^eos$"
+                nextWord2 = "^eos$"
+            else:
+                nextWord1 = str(splitLine[i+1])
+                nextWordTag1 = tagList[i+1]
+                nextWordClass1 = str(nextWordTag1[1])
+                if (i == splitLength - 2):
+                    nextWord2 = "^eos$"
+                else:
+                    nextWordTag2 = tagList[i+1]
+                    nextWordClass2 = str(nextWordTag2[1])
+                    nextWord2 = str(splitLine[i+2])
+
+            # create outTagLine and write;
+            tagLine += " "+"prev2:"+prevWord2+" "+"prevClass2:"+prevClassLabel2+" "+"prev1:"+prevWord1+" "+"prevClass1:"+prevClassLabel1+" "+"curClass:"+curClassLabel +" "+"suffix2:"+suffix2+" "+"suffix3:"+suffix3+" " + "next1:"+nextWord1+" " +"nextClass1:"+nextWordClass1+" "+ "next2:"+nextWord2+" "+"nextClass2:"+nextWordClass2
+            outTagLine = "test" + tagLine
+            outList.append(outTagLine)
+            ou.write(outTagLine + "\n")
+
+    tr.close()
+    ou.close()
+    return outList
+
+
+def getMegamPrediction(modelFile,formatFile,megamPath):
+    # print("megam")
+    megamString = megamPath+" -nc -predict " + modelFile + " multitron " + formatFile
+    (stdout,stderr) = subprocess.Popen(megamString,stdout=subprocess.PIPE,stderr=subprocess.PIPE,shell=True).communicate()
+    # print(stderr)
+    if isinstance(stdout, compat.string_types):
+        return stdout
+    else:
+        return stdout.decode('utf-8')
+
+
+def reconstructOutput(smout,tFile,mSet):
+
+    predTagList = []
+    reconList = smout.split("\n")
+    # print(reconList[0])
+    for reconLine in reconList:
+        predTag = reconLine.split("\t")[0]
+        predTagList.append(str(predTag))
+
+
+    trList = []
+    trf = open(tFile,'r')
+    for line in trf:
+        trList.append(line)
+    trf.close()
+
+    c = 0
+    for trLine in trList:
+        outLine = ""
+        stripLine = trLine.strip("\n")
+        trainWords = stripLine.split()
+        # iterate words in the list;
+        for w in range(len(trainWords)):
+            if str(trainWords[w]) in mSet:
+                outLine += (" " + predTagList[c])
+                c += 1
+            else:
+                outLine += (" " + trainWords[w])
+        print(outLine)
+    
+    return predTagList
+
+
+def initSet():
+    mainSet = set()
+    mainSet.add("its")
+    mainSet.add("it\'s")
+    mainSet.add("they\'re")
+    mainSet.add("their")
+    mainSet.add("your")
+    mainSet.add("you\'re")
+    mainSet.add("too")
+    mainSet.add("to")
+    mainSet.add("lose")
+    mainSet.add("loose")
+    return mainSet
+
+
+
+if __name__ == '__main__':
+
+    megamPath = "./megam_0.92/./megam"
+    modelFile = sys.argv[1]
+    testFile = sys.argv[2]
+
+    testFileFormat = "./hw3.test.format.txt"
+
+    mainSet = initSet()
+
+    testList = formatInput(testFile,testFileFormat,mainSet)
+    mOut = getMegamPrediction(modelFile,testFileFormat,megamPath)
+    predList = reconstructOutput(mOut,testFile,mainSet)
+
+
---
+++ src/learn.py
@@ -0,0 +1,143 @@
+import nltk,subprocess,sys
+from nltk.corpus import brown
+
+def getSuffix(word,type):
+    wlen = len(word)
+    return word[wlen-type:wlen]
+
+def format(out,tr):
+    outList = []
+    ou = open(out,'w')
+    # tr = open(train,'r')
+    for line in tr:
+        # print(".")
+        # Skipping
+        splitLineSet = set()
+        for itemTuple in line:
+            splitLineSet.add(str(itemTuple[0]))
+
+        if ("its" not in splitLineSet and
+                    "it\'s" not in splitLineSet and
+                    "too" not in splitLineSet and
+                    "to" not in splitLineSet and
+                    "your" not in splitLineSet and
+                    "you\'re" not in splitLineSet and
+                    "their" not in splitLineSet and
+                    "they\'re" not in splitLineSet and
+                    "loose" not in splitLineSet and
+                    "lose" not in splitLineSet):
+            # print("skipped")
+            continue
+
+        splitLength = len(line)
+        prevWord1 = "^bos$"
+        prevWord2 = "^bos$"
+        prevClass = "^cla$$"
+        curWord = ""
+        nextWord = ""
+        curClassLabel = ""
+        suffix2 = ""
+        suffix3 = ""
+
+        for i in range(splitLength):
+            tagLine = ""
+            outTagLine = ""
+            splitLine = line[i]
+            # handle cur;
+            if ("its" != splitLine[0] and
+                        "it\'s" != splitLine[0] and
+                        "too" != splitLine[0] and
+                        "to" != splitLine[0] and
+                        "your" != splitLine[0] and
+                        "you\'re" != splitLine[0] and
+                        "their" != splitLine[0] and
+                        "they\'re" != splitLine[0] and
+                        "loose" != splitLine[0] and
+                        "lose" != splitLine[0]):
+                # print("skipped Word")
+                continue
+
+            # handle cur word;
+            curWord = str(splitLine[0])
+            suffix2 = getSuffix(curWord,2)
+            suffix3 = getSuffix(curWord,3)
+            curWordTag = splitLine[1]
+            curClassLabel = str(curWordTag)
+
+            #handle prev word;
+            if(i == 0):
+                prevWord1 = "^bos$"
+                prevWord2 = "^bos$"
+            elif (i == 1):
+                prevWord2 = "^bos$"
+            else:
+                prevTuple1 = line[i-1]
+                prevWord1 = str(prevTuple1[0])
+                prevWordTag1 = str(prevTuple1[1])
+
+                prevTuple2 = line[i-2]
+                prevWord2 = str(prevTuple2[0])
+                prevWordTag2 = str(prevTuple2[1])
+
+                prevClassLabel1 = str(prevWordTag1)
+                prevClassLabel2 = str(prevWordTag2)
+
+            # handle next;
+            if (i  == splitLength - 1):
+                nextWord1 = "^eos$"
+                nextWord2 = "^eos$"
+            else:
+                nextTuple1 = line[i+1]
+                nextWord1 = str(nextTuple1[0])
+                nextWordTag1 = str(nextTuple1[1])
+                nextWordClass1 = str(nextWordTag1)
+
+                if (i == splitLength - 2):
+                    nextWord2 = "^eos$"
+                else:
+                    nextTuple2 = line[i+2]
+                    nextWord2 = str(nextTuple2[0])
+                    nextWordTag2 = str(nextTuple2[1])
+                    nextWordClass2 = str(nextWordTag2)
+
+
+            # create outTagLine and write;
+            tagLine += " "+"prev2:"+prevWord2+" "+"prevClass2:"+prevClassLabel2+" "+"prev1:"+prevWord1+" "+"prevClass1:"+prevClassLabel1+" "+"curClass:"+curClassLabel +" "+"suffix2:"+suffix2+" "+"suffix3:"+suffix3+" " + "next1:"+nextWord1+" " +"nextClass1:"+nextWordClass1+" "+ "next2:"+nextWord2+" "+"nextClass2:"+nextWordClass2
+            outTagLine = curWord + tagLine
+            outList.append(outTagLine)
+            ou.write(outTagLine + "\n")
+
+    # tr.close()
+    ou.close()
+    return outList
+
+
+def readBrown():
+    sentences = [ (sent)
+                  for genre in brown.categories()
+                  for sent in brown.tagged_sents(categories = genre)
+    ]
+    # print(len(sentences))
+    # print(sentences[0])
+    return sentences
+
+
+def generateModel(formatFile,modelFile,megamPath):
+    megamString = megamPath+" -nc multitron "+formatFile +" > "+modelFile
+    p = subprocess.Popen(megamString,stdout=subprocess.PIPE,stderr=subprocess.PIPE,shell=True).communicate()[0]
+
+
+if __name__ == '__main__':
+
+    megamPath = "./megam_0.92/./megam"
+
+    train_out = "./hw3.train.brown.out"
+
+    modelFile = sys.argv[1]
+
+    brownSentTagged = readBrown()
+    trainList = format(train_out,brownSentTagged)
+    generateModel(train_out,modelFile,megamPath)
+
+
+
